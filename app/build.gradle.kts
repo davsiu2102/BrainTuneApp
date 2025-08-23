ca1ad9a5292c74b3ce8ec914ab8e25d73b9095dc
davsiu21 <siumandav@gmail.com>
2025-08-22 20:18:10 -0600

    correción de error id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.braintune_app_"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.braintune_app_"
